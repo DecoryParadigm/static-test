a6f77b1f5e2dc747e5d9418ccdc7c5827668a500
Decory <decory@Decorys-iMac.speakertech.local>
2022-07-26 09:19:08 -0400

    position change

--- static-header.js
+++ static-header.js
@@ -12,4 +12,6 @@ basic_header.className = "header"
 basic_header.style.width = "100%";
 basic_header.style.height = "20%";
 basic_header.style.backgroundColor = "red"
+basic_header.style.position = "absolute"
+basic_header.style.zIndex = "900"
 
