ecb3eaa21e0a1d9c29e61dc4bbdf2b95590b3d31
Decory <decory@Decorys-iMac.speakertech.local>
2022-07-26 09:21:36 -0400

    added top left

--- static-header.js
+++ static-header.js
@@ -1,17 +1,18 @@
 
-
-
-
-
-
-
-
 // Template Header - Testing with tailwind
+
+
+
+
+
+
 const basic_header = document.createElement('div');
 basic_header.className = "header"
 basic_header.style.width = "100%";
 basic_header.style.height = "20%";
 basic_header.style.backgroundColor = "red"
 basic_header.style.position = "absolute"
+basic_header.style.top = "0"
+basic_header.style.left = "0"
 basic_header.style.zIndex = "900"
 
