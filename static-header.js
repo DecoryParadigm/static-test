8079ef13ca9087950834a3a133281af639d7d66c
Decory <decory@Decorys-iMac.speakertech.local>
2022-07-26 09:06:01 -0400

    initial test commit

---
+++ static-header.js
@@ -0,0 +1,5 @@
+// Template Header - Testing with tailwind
+const basic_header = document.createElement('div');
+basic_header.style.width = "100%";
+basic_header.style.height = "20%";
+basic_header.style.backgroundColor = "red"
