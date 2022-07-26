e72fbeec8864551d11a058bdcafdf537ee5e0f88
Decory <decory@Decorys-iMac.speakertech.local>
2022-07-26 09:40:01 -0400

    hello world

--- static-header.js
+++ static-header.js
@@ -3,15 +3,17 @@
 
 
 window.onload = () => { 
-    const basic_header = document.createElement('div');
-    basic_header.className = "header"
-    basic_header.style.width = "100%";
-    basic_header.style.height = "20%";
-    basic_header.style.backgroundColor = "red"
-    basic_header.style.position = "absolute"
-    basic_header.style.top = "0"
-    basic_header.style.left = "0"
-    basic_header.style.zIndex = "900"
+
+    alert("hello world")
+    // const basic_header = document.createElement('div');
+    // basic_header.className = "header"
+    // basic_header.style.width = "100%";
+    // basic_header.style.height = "20%";
+    // basic_header.style.backgroundColor = "red"
+    // basic_header.style.position = "absolute"
+    // basic_header.style.top = "0"
+    // basic_header.style.left = "0"
+    // basic_header.style.zIndex = "900"
 
 }
 
