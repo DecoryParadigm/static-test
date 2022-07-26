9f419345ffb146c20bfa677c351465525545e42e
Decory <decory@Decorys-iMac.speakertech.local>
2022-07-26 09:37:16 -0400

    missed onload func

--- static-header.js
+++ static-header.js
@@ -2,17 +2,22 @@
 // Template Header - Testing with tailwind
 
 
+window.onload = () => { 
+    const basic_header = document.createElement('div');
+    basic_header.className = "header"
+    basic_header.style.width = "100%";
+    basic_header.style.height = "20%";
+    basic_header.style.backgroundColor = "red"
+    basic_header.style.position = "absolute"
+    basic_header.style.top = "0"
+    basic_header.style.left = "0"
+    basic_header.style.zIndex = "900"
+
+}
 
 
 
 
-const basic_header = document.createElement('div');
-basic_header.className = "header"
-basic_header.style.width = "100%";
-basic_header.style.height = "20%";
-basic_header.style.backgroundColor = "red"
-basic_header.style.position = "absolute"
-basic_header.style.top = "0"
-basic_header.style.left = "0"
-basic_header.style.zIndex = "900"
+
+
 
