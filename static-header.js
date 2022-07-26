16b1a3ef2796f14c7b195ab4cb655ea9a952f907
Decory <decory@Decorys-iMac.speakertech.local>
2022-07-26 09:15:20 -0400

    add class

--- static-header.js
+++ static-header.js
@@ -1,5 +1,15 @@
+
+
+
+
+
+
+
+
 // Template Header - Testing with tailwind
 const basic_header = document.createElement('div');
+basic_header.className = "header"
 basic_header.style.width = "100%";
 basic_header.style.height = "20%";
 basic_header.style.backgroundColor = "red"
+
